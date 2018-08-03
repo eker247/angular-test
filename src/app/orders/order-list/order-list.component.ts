1092d0176a3c84117002763497b8b21b7ab1cc5f
Krzysztof Fornagiel <kfornagiel@fakro.pl>
2018-08-03 15:04:28 +0200

    nested forms

--- src/app/orders/order-list/order-list.component.ts
+++ src/app/orders/order-list/order-list.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormArray, FormBuilder } from '@angular/forms';
+
+export interface Customer {
+  name: string; // required field with minimum 5 characters
+  addresses: Address[]; // user can have one or more addresses
+}
+
+export interface Address {
+  street: string;  // required field
+  postcode: string;
+}
 
 @Component({
   selector: 'app-order-list',
@@ -6,10 +17,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-list.component.css']
 })
 export class OrderListComponent implements OnInit {
+  public myForm: FormGroup; // our form model
 
-  constructor() { }
+  // we will use form builder to simplify our syntax
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit() {
+    this.myForm = this._fb.group({
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      addresses: this._fb.array([
+          this.initAddress(),
+        ])
+    });
   }
 
+  initAddress() {
+    // initialize our address
+    return this._fb.group({
+        street: ['', Validators.required],
+        postcode: ['']
+    });
+  }
+
+  addAddress() {
+    // add address to the list
+    const control = <FormArray>this.myForm.controls['addresses'];
+    control.push(this.initAddress());
+  }
+
+  removeAddress(i: number) {
+    // remove address from the list
+    const control = <FormArray>this.myForm.controls['addresses'];
+    control.removeAt(i);
+  }
+
+  save(model: Customer) {
+    // call API to save customer
+    console.log(model);
+  }
 }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrderListComponent } from './order-list/order-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [OrderListComponent]
 })
